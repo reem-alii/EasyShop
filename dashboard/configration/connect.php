96be8c4c7dcb325d83e2f0e33bfc4e3053cf696d
reem-alii <r25786783@gmail.com>
2025-04-09 17:18:20 +0200

    last

--- dashboard/configration/connect.php
+++ dashboard/configration/connect.php
@@ -1,7 +1,7 @@
 <?php
  $dsn = 'mysql:host=localhost;dbname=easy_shop';
- $username = 'myuser';
- $password = 'reemaliiii';
+ $username = '';
+ $password = '';
  $options = array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8');
 
  try {
